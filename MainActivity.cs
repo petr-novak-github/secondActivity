6ecf4941c5a692db4b1d5171becdc1aa47e257ef
Petr Novak <petr-novak-donald@protonmail.com>
2021-04-18 14:37:25 +0200

    upload files

---
+++ MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+using Android.Content;
+using System;
+
+namespace SecondActivityApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private Button _tlacitko1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+         
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            SetupReferencesPeno();
+            SubscribeHandlersPeno();
+           
+        }
+        private void SetupReferencesPeno()
+        {
+            
+            _tlacitko1 = FindViewById<Button>(Resource.Id.button1);
+        }
+
+        private void SubscribeHandlersPeno()
+        {
+            _tlacitko1.Click += Tlacitko1_Click;
+            
+        }
+
+
+        private void Tlacitko1_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(SecondActivity));
+            StartActivity(intent);
+        }
+    }
+}
---
+++ SecondActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SecondActivityApp
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.second_activity);
+        }
+    }
+}
